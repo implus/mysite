accc18c82abde1cc749a80862098a95d7730c775
implus <674592809@qq.com>
2013-06-11 00:51:13 +0800

    change urls

--- urls.py
+++ urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import *
 from views import hello, current_datetime, hours_ahead,display_meta
-
-import books.views
-import contact.views
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,9 +9,13 @@ urlpatterns = patterns('',
     (r'^time/$', current_datetime),
     (r'^time/plus/(\d{1,2})/$', hours_ahead),
     (r'^lx/',display_meta),
-    #(r'^search-form/$', views.search_form),
-    (r'^search/$',books.views.search),
-    (r'^contact/$',contact.views.contact),
-    (r'^contact/thanks/$',contact.views.thanks),
 )
 
+urlpatterns += patterns('books.views',
+    (r'^search/$','search'),
+)
+
+urlpatterns += patterns('contact.views',
+    (r'^contact/$','contact'),
+    (r'^contact/thanks/$','thanks'),
+)
